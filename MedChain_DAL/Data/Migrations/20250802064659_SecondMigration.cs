8efb8a94ba20a4133471a190e563b667413012fa
M-Aitisam <aitisamahmed24@gmail.com>
2025-08-02 11:52:13 +0500

    Module 2 Part 5  Fix the issue and working on  to  store register user in the Database

---
+++ MedChain_DAL/Data/Migrations/20250802064659_SecondMigration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedChain_DAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "0ac103fb-38ea-4429-a426-bf66fe8b72f2");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "fa9fe980-6d2c-4e6e-beb9-e089abcdcb2c");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "becd1e4f-4282-4468-bd55-ab318a12723f");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "4b3a4427-202c-4431-9095-d0c2af0004c9");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "7bf4bd5f-16cb-4536-a6d2-bf11ec813433");
+
+            migrationBuilder.UpdateData(
+                schema: "dbo",
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "68a76954-7d8c-45ca-8aaf-8d7449fcc6ae");
+        }
+    }
+}
